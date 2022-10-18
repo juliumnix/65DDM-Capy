fcb98461910daee2581c1eeb199bb4e0906a2454
juliumnix <juliumnix@gmail.com>
2022-10-17 21:01:24 -0300

    adicionando fragments

--- app/src/main/java/com/example/capy_65ddm/HomeScreen.java
+++ app/src/main/java/com/example/capy_65ddm/HomeScreen.java
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
 
 import com.example.capy_65ddm.databinding.ActivityHomeScreenBinding;
 
@@ -17,9 +21,12 @@ public class HomeScreen extends AppCompatActivity {
         binding = ActivityHomeScreenBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
-        replaceFragment(new MensagensFragment());
+        replaceFragment(new HomeFragment());
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
                 case R.id.mensagens:
                     replaceFragment(new MensagensFragment());
                     break;
@@ -40,4 +47,5 @@ public class HomeScreen extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
+
 }
--- app/src/main/java/com/example/capy_65ddm/MainActivity.java
+++ app/src/main/java/com/example/capy_65ddm/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.capy_65ddm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,6 +18,18 @@ public class MainActivity extends AppCompatActivity {
 //
 
         setContentView(R.layout.activity_main);
+
+        //funcao temporaria, aqui vai consultar o banco de dados e depois fazer a navegacao
+        new android.os.Handler(Looper.getMainLooper()).postDelayed(
+                new Runnable() {
+                    public void run() {
+                        Intent in = new Intent(MainActivity.this, LoginScreen.class);
+                        in.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(in);
+                        setContentView(R.layout.activity_main);
+                    }
+                },
+                3000);
 
     }
 }
